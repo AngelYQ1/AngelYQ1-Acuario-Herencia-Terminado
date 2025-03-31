d6c848b7b897fc0eb7063a796a313f2b1f2155bd
Angel Yucra <angelyucra43@gmail.com>
2025-03-31 00:45:11 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/main.kt
@@ -0,0 +1,33 @@
+package acuario
+
+fun construirAcuario() {
+    val acuario1 = Acuario()
+    acuario1.imprimirTamano()
+
+    val acuario2 = Acuario(ancho = 25)
+    acuario2.imprimirTamano()
+
+    val acuario3 = Acuario(alto = 35, largo = 110)
+    acuario3.imprimirTamano()
+
+    val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+    acuario4.imprimirTamano()
+
+    val acuario5 = Acuario(numeroDePeces = 29)
+    acuario5.imprimirTamano()
+
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+
+    fun construirAcuario() {
+        val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+        miAcuario.imprimirTamano()
+
+        val miTorre = TanqueTorre(diametro = 25, alto = 40)
+        miTorre.imprimirTamano()
+    }
+}
+
+fun main() {
+    construirAcuario()
+}
